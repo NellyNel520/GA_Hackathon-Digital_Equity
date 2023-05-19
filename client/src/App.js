a281632dd06756fa0dcc2aa92970be6bb19fee0b
Marcus Hudnell <103913070+hudnellmarcus@users.noreply.github.com>
2023-05-19 13:37:26 -0700

    initial firebase auth

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,41 @@
 import './index.css';
+import app from './firebase_config';
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth'; 
+import { useState } from 'react';
+
+const auth = getAuth(app); 
+
 
 function App() {
+  
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState(""); 
+  
+  const handleSignUp = () => {
+    const onHandleSignup = async () => {
+      try {
+        if (email !== '' && password !== '') {
+          await auth.createUserWithEmailAndPassword(email, password);
+        }
+      } catch (error) {
+        setSignupError(error.message);
+      }
+    };
+  
+  };
+
+  const onLogin = async () => {
+    try {
+        if (email !== '' && password !== '') {
+            await auth.signInWithEmailAndPassword(email, password);
+        }
+    } catch (error) {
+        setLoginError(error.message);
+    }
+};
+
+
+
   return (
     <div className="App">
       App
