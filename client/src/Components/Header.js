1c07557f795b431dedb2023f12e823231368692d
NellyNel520 <channelharris520@gmail.com>
2023-05-20 19:04:41 -0400

    moved images folder

--- client/src/Components/Header.js
+++ client/src/Components/Header.js
@@ -1,8 +1,22 @@
 import React from 'react'
+import styled from 'styled-components'
+import MaskGroup from '../../public/assets/MaskGroup.png'
+
+const Container = styled.div`
+	width: 100vw;
+	height: 100vh;
+	display: flex;
+	position: relative;
+	overflow: hidden;
+  background-image: url("../../public/assets/MaskGroup.png") 
+	${'' /* ${mobile({ display: "none" })} */}
+`
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div>
+    <Container >Header</Container>
+    </div>
   )
 }
 
