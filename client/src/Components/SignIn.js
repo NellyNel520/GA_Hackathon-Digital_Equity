5d53bc9c4ef2a4a94cc5dfb63d2bf3404733e451
Marcus Hudnell <103913070+hudnellmarcus@users.noreply.github.com>
2023-05-20 12:59:24 -0700

    merged login and signin.js

--- client/src/Components/SignIn.js
+++ client/src/Components/SignIn.js
@@ -1,18 +1,38 @@
+import {auth} from '../firebase';
+import { signInWithEmailAndPassword } from "firebase/auth";
 
 
 
 export const SignIn=()=>{
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+
+  const handleLogin = async () => {
+    try {
+        if (email !== '' && password !== '') {
+            await signInWithEmailAndPassword(auth, email, password);
+        }
+    } catch (error) {
+        console.log(error.message);
+    }
+  };
+
     return(
         <div className="border-2 border-cyan-800 border-radius:0.25rem">
             <h1>Company</h1>
             <h2>Welcome</h2>
             <h3>Sign in to Company</h3>
-        <form>
+        <form
+        onSubmit={handleLogin} >
             <input
               id='username'
               type='text'
               name="username"
               placeholder="Username"
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
               required
             />
             <input
@@ -20,6 +40,8 @@ export const SignIn=()=>{
               type='password'
               name="password"
               placeholder="Password"
+              onChange={(e) => setPassword(e.target.value)}
+              value={password}
               required
             />
             
