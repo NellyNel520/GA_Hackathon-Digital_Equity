4d7e909317639876fbc970f034bb15ceb61ff878
mahamakhan <mahamimranamjad@gmail.com>
2023-05-20 10:28:26 -0700

    accomodations

---
+++ client/src/Components/Accomodations.js
@@ -0,0 +1,13 @@
+export const Accomodations = () => {
+    <div>
+      <button>Computer Text-To-Speech</button>
+      <button>Color Blindness</button>
+      <button>Sign Language Interpretations</button>
+      <button>Language Translation</button>
+      <button>Hearing Loss</button>
+      <button>Visual Motor Deficit</button>
+      <button>Autism Spectrum Disorder</button>
+      <button>None Applicable</button>
+      <button>Back</button>
+    </div>
+  }
