56aec7e4762e32672a5a9cbe5411c942cfd6f5d6
NellyNel520 <channelharris520@gmail.com>
2023-05-21 10:38:53 -0400

    home page complete

--- client/src/Components/Header.js
+++ client/src/Components/Header.js
@@ -1,23 +1,41 @@
 import React from 'react'
 import styled from 'styled-components'
-import MaskGroup from '../../public/assets/MaskGroup.png'
+import MaskGroup from '../assets/MaskGroup.png'
 
 const Container = styled.div`
 	width: 100vw;
-	height: 100vh;
+	${'' /* height: 100vh; */}
 	display: flex;
 	position: relative;
 	overflow: hidden;
-  background-image: url("../../public/assets/MaskGroup.png") 
+	${'' /* background-image: url{'src/assets/MaskGroup.png'} */}
 	${'' /* ${mobile({ display: "none" })} */}
 `
 
 const Header = () => {
-  return (
-    <div>
-    <Container >Header</Container>
-    </div>
-  )
+	return (
+		<div>
+			<Container className="bg-blue-300">
+				<div className="flex">
+					<div className="w-[45%] mt-[6rem] m-6 ml-[5rem]">
+						<h1 className='text-[2.5rem] text-white font-bold mb-2'>Teach E-Learning: Empowering Young Innovators for the Future!</h1>
+						<p className='mb-3'>
+							Discover the thrilling world of technology with Engaging
+							E-Learning Courses Designed just for kids
+						</p>
+            <div>
+            <button className='border rounded bg-[#3a69bb] py-2 px-4 text-white'>Get Started</button>
+          </div>
+					</div>
+
+					<div>
+						<img src={MaskGroup} alt="ll" />
+					</div>
+          
+				</div>
+			</Container>
+		</div>
+	)
 }
 
 export default Header
