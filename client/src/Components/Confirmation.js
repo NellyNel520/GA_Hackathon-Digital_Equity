54cc347595d5944c269eaba88845122ed033a585
mahamakhan <mahamimranamjad@gmail.com>
2023-05-21 12:41:30 -0700

    added buttons to each file

--- client/src/Components/Confirmation.js
+++ client/src/Components/Confirmation.js
@@ -1,11 +1,15 @@
 import confirm from '../assets/41.png'
 import '../Css/Confirmation.css'
+import { Back } from './Back'
+import { Continue } from './Continue'
 
 export const Confirmation=()=>{
     return(
         <div>
             <img src={confirm} alt="image" className="picture"/>
             <h3 className='wait'>Just a second! We're putting together your personalised curriculum....</h3>
+            <Back/>
+            
         </div>
     )
 }
