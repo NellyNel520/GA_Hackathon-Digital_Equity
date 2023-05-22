4a73ec367efd45a63f899e2f8121bae295d87b4f
mahamakhan <mahamimranamjad@gmail.com>
2023-05-22 11:54:41 -0700

    first sign up changed to the design layout

--- client/src/Components/FirstSignup.js
+++ client/src/Components/FirstSignup.js
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import logo from '../assets/SignInScreen1.png'
+import '../Css/SignIn.css'
 
 export const FirstSignup = ({ onContinue }) => {
     const [role, setRole] = useState("");
@@ -13,16 +15,58 @@ export const FirstSignup = ({ onContinue }) => {
     };
 
     return (
-        <div>
-            <h1>Web Wizards</h1>
-            <h3>Welcome!</h3>
-            <h5>I am a...</h5>
-            <button onClick={() => handleRoleSelection("School")}>School</button>
-            <button onClick={() => handleRoleSelection("Parent")}>Parent</button>
-            <button onClick={() => handleRoleSelection("Student")} >Student</button>
-            <h2>OR</h2>
-            <button>Contine with Google</button>
-            <h3>Already have an account? Sign In</h3>
+        <div className="flex mx-[10rem] h-[90vh] mt-10 pb-[3rem]">
+    <div className="my-8">
+      <img className="w-[70%]" src={logo} alt="backgroundpic" />
+    </div>
+    <div className="bg-[#E0FBFC] w-[50%] h-[40rem] my-5 rounded my-8 pb-10">
+      <div>
+        {/* <h1 className="techsavvy">TECHSAVVY</h1> */}
+        <h1 className="text-center text-[3rem] mt-4"><span className='text-orange-500'>TECH</span>SAVVY</h1>
+
+        
+        {/* <h2 className="welcomeback">Welcome Back!</h2> */}
+        <p className="text-center text-[2rem]">Welcome!</p>
+
+        <form className='ml-4'>
+          <input
+            id='username'
+            type='text'
+            name="username"
+            placeholder="Username"
+            // onChange={(e) => setEmail(e.target.value)}
+            // value={email}
+            required
+          />
+          <input
+            id='password'
+            type='password'
+            name="password"
+            placeholder="Password"
+            // onChange={(e) => setPassword(e.target.value)}
+            // value={password}
+            required
+          />
+          <input
+            id='password'
+            type='password'
+            name="password"
+            placeholder="Re-enter password"
+            // onChange={(e) => setPassword(e.target.value)}
+            // value={password}
+            required
+          />
+          <div className='text-center ml-[6rem] mt-10'>
+    
+          </div>
+          
+          </form>
+          <button className="create" onClick={() => handleRoleSelection("Student")} >Create an Account</button>
+      </div>
+    </div>
+            
+            
+           
         </div>
     )
 };
